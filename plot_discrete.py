27887ee3a0c88eccbeac751fbf76215e2c67245f
Jez <jezdavisuk@gmail.com>
2024-11-10 16:54:50 +0000

    feat: add logic to function discrete_quadratic() discretises signal V(t) over two-second intervals

--- plot_discrete.py
+++ plot_discrete.py
@@ -5,3 +5,14 @@ def discrete_quadratic(T,N,tN):
     # create an array of zeros of length N
     V = np.zeros(N, dtype=np.float32)
 
+# discretise time
+    for k in range(0,N):
+        dt = tN / N     # define time-step
+        tk = k * dt     # discrete time an integer multiple of time-step
+
+        # define V(t) on t in [0,2]
+        if (tk % T) < T / 2:
+            V[k] = (tk % T) ** 2
+        else:
+            V[k] = ((tk % T) - T) ** 2
+    return V
